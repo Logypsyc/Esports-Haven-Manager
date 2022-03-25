0aad6129c37fe9c835d03b29560e7087be1ebe51
Kenneth Lin <70595901+Logypsyc@users.noreply.github.com>
2022-03-24 21:29:24 -0400

    Update music.py

--- music.py
+++ music.py
@@ -21,15 +21,26 @@ class music(commands.Cog):
     await ctx.voice_client.disconnect()
 
   @commands.command()
-  async def play(self, ctx, url):
+  async def play(self, ctx, *, url):
+    if ctx.author.voice is None:
+      await ctx.send("You are not in a voice channel!")
+    voice_channel = ctx.author.voice.channel
+    if ctx.voice_client is None:
+      await voice_channel.connect()
+    else:
+      await ctx.voice_client.move_to(voice_channel)
+
     ctx.voice_client.stop()
     FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-    YDL_OPTIONS = {'format':"bestaudio"}
+    YDL_OPTIONS = {'format':"bestaudio", 'default_search':"ytsearch"}
     vc = ctx.voice_client
 
     with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
-      info = ydl.extract_info(url, download = False)
-      url2 = info['formats'][0]['url']
+      info = ydl.extract_info(url, download=False)
+      if 'entries' in info:
+        url2 = info["entries"][0]["formats"][0]['url']
+      elif 'formats' in info:
+        url2 = info["formats"][0]['url']
       source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
       vc.play(source)
 
