3eb4d81f273349bbaab8f596d10c555cfcd5a4d9
Kenneth Lin <70595901+Logypsyc@users.noreply.github.com>
2023-01-04 12:24:48 -0500

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from waitress import serve
 from threading import Thread
 
 app = Flask('')
@@ -8,7 +9,7 @@ def home():
     return "Hello. I am alive!"
 
 def run():
-  app.run(host='0.0.0.0', port=8080)
+  serve(app, host='0.0.0.0', port=8080)
 
 def keep_alive():
     t = Thread(target=run)
